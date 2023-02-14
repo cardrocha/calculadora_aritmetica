57172ffda20c466d5918a1ef7161a109a7fcd1ef
Ricardo Rocha <cardrocharj@gmail.com>
2023-02-14 18:29:51 -0300

    fix code app.vue

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,9 @@
 import { reactive } from 'vue';
 
 const estado = reactive ({
-  operacao: '',
+  operacao: 'adicao',
   numb1: 0,
   numb2: 0,
-  // value: '',
 })
 
 const operacaoSoma = () => {
@@ -25,30 +24,21 @@ const operacaoDivisao = () => {
   return estado.numb1 / estado.numb2;
 }
 
-// const getOperacaoSelecionada = () => {
-//   const {operacao} = estado;
+const getOperacaoSelecionada = () => {
+  const {operacao} = estado;
 
-//   switch (operacao) {
-//     case 'adicao':
-//       return  operacaoSoma();
-//     case 'subtracao':
-//       return operacaoSubtracao();
-//     case 'multiplicacao':
-//       return operacaoMultiplicacao();
-//     case 'divisao':
-//       return operacaoDivisao();
-//   }
-//   return resultado;
-// }
+  switch (operacao) {
+    case 'adicao':
+      return  operacaoSoma();
+    case 'subtracao':
+      return operacaoSubtracao();
+    case 'multiplicacao':
+      return operacaoMultiplicacao();
+    case 'divisao':
+      return operacaoDivisao();
+  }
+}
 
-// function campoSelect(result) {
-//   let select = document.querySelector('#opcao');
-//   let optionValue = select.options[select.selectedIndex];
-  
-//   let value = optionValue.value;
-//   let resultado = optionValue.text;
-  
-// }
 
 </script>
 
@@ -56,7 +46,7 @@ const operacaoDivisao = () => {
   <header class="p-5 mb-4 mt-4 bg-dark rounded-3">
     <div class="container">
       <h1>Calculadora Aritmética</h1>
-      <!-- <span class="p-2 mb-2 mt-2 bg-light rounded-3 col-md-1">{{  }}</span> -->
+      <span class="p-2 mb-2 mt-2 bg-light rounded-3 col-md-1">{{ getOperacaoSelecionada() }}</span>
     </div>
   </header>
   <form>
@@ -68,20 +58,24 @@ const operacaoDivisao = () => {
         <input type="number" @change="evento => estado.numb2 = evento.target.value" placeholder="insira um número" class="form-control">
       </div>
       <div class="col-md-3">
-        <select id="opcao" class="form-control text-center">
+        <select id="opcao" class="form-control text-center" @change="evento => estado.operacao = evento.target.value">
           <option selected disabled value="">Escolha a operação</option>
-          <option>(+) Adição = {{ operacaoSoma() }}</option>
-          <option>(-) Subtração = {{ operacaoSubtracao() }}</option>
-          <option>(*) Multiplicação = {{ operacaoMultiplicacao() }}</option>
-          <option>(÷) Divisão = {{ operacaoDivisao() }}</option>
+          <option value="adicao">(+) Adição</option>
+          <option value="subtracao">(-) Subtração</option>
+          <option value="multiplicacao">(*) Multiplicação</option>
+          <option value="divisao">(÷) Divisão</option>
         </select>
+        </div>
       </div>
-    </div>
   </form>
 </template>
 
 <style scoped>
 .container {
   color: #fff;
+}
+
+span {
+  color: #000;
 }
 </style>
